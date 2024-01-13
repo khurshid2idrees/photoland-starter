8e29f394c29869069cfd03589a666bbce7fb110b
Khurshid2idrees <umarkhurshid3@gmail.com>
2024-01-13 15:48:34 +0530

    updpate in home product and productslider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,13 +7,13 @@ import ProductSlider from "../components/ProductSlider.js";
 
 const Home = () => {
   const { data } = useFetch("/products?populate=*&filters[isNew]=true");
-  console.log(data);
+  // console.log(data);
 
   return (
     <div className="mb-16">
      
     <div className="container mx-auto">
-      <h2 className="h2 mb-6 text-center xl:text-left"> Latest Products</h2>
+      <h2 className="h2 mb-6 text-left xl:text-center"> Latest Products</h2>
     </div>
 
       <ProductSlider data={data} />
